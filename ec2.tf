8755c1970da61eab938f81ee1a8883d0ca4289f7
sayantan288 <44430957+sayantan288@users.noreply.github.com>
2021-09-05 13:07:21 +0530

    update

--- ec2.tf
+++ ec2.tf
@@ -5,9 +5,10 @@ provider "aws" {
 resource "aws_instance" "myec2" {
   ami = "ami-0ab4d1e9cf9a1215a"
   instance_type = "t2.micro"
-  count = 3
+  key_name = "mykey"
+}
 
-  tags = {
-    Name = var.instance-name[count.index]
-  }
+resource "aws_key_pair" "mykey" {
+  key_name   = "mykey"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCRrlPP/HeehYpiZUfZNDQGyyb5bvpJqv/wBNYmq8CN3jIBe0i62QXs3TJaAFhsAtdKg4s/WW1C1YzRAXjFrS34rVHMmC9aN/Fy419VFXns4p+7gxawAcHSahO7G/UMi51kaKYahRuBKbCYVuxfyliNZphUb9v8z8MABEQLcmoE3GF/IlKjwZ4ayLK+odJpijl6Qzt8BEKPgD3UtAXC6mz8SvXvWti2ZNXYfAcICRkbLRt4odu7aXg801bhtDkkFfPhph0Ef0XPX4uzzTi7nQgdMScZRvGaaOhInXQJhEQef2DmPpdqNgEbqLch2zFTL+BOg43/9qU4nSDOLWWDvJzKuLVFTeiQi1gLIwnmA9kM/5BTTKecXvCPRn/EiaDbBqyJIdy47LBpDoEDCzJ2yjj8ZomE2XXuEI45bc61gHPbGBsrGwGS+LjlZcIEzeMEWQIlvhgZLeNh8suikYU8K3jlswhDHhLdpw6xsPWKbwwLJVbtsKpVsFzG++5UDSfF2EU= Sayantan@Sayantan-PC"
 }
